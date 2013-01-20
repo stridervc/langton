b664c2e98535c5465b3b3947df8a1271b07d1eda
Fred Strauss <fred@strider.co.za>
2013-01-20 19:03:29 +0200

    basic zoom works

--- langton.cpp
+++ langton.cpp
@@ -5,26 +5,28 @@
 
 #define WIDTH 1024
 #define HEIGHT 600
-
-// field size
 #define FWIDTH 512
 #define FHEIGHT 300
+#define ZOOM 4
 
 void	putpixel(SDL_Surface *surface, int x, int y, int r, int g, int b);
 void	field2surface(char *field, SDL_Surface *surface);
 void	heatmap2surface(unsigned int *heatmap, SDL_Surface *surface);
-void	updatescreen(SDL_Surface *screen, SDL_Surface *field, SDL_Surface *heatmap);
+void	updatescreen(SDL_Surface *screen, SDL_Surface *field, SDL_Surface *zoom, SDL_Surface *heatmap);
+void	updateZoom(int antx, int anty, char *field, SDL_Surface *zoomSurface, int &zoomx, int &zoomy);
 
 int main(int argc, char **argv) {
 	SDL_Surface			*screen = NULL;			// Whole screen
 	SDL_Surface			*fieldSurface = NULL;
 	SDL_Surface			*heatmapSurface = NULL;
+	SDL_Surface			*zoomSurface = NULL;
 	SDL_Event			event;
 
 	bool				quit = false;
 	char				field[FWIDTH*FHEIGHT];	// the playing field
 	unsigned int		heatmap[FWIDTH*FHEIGHT];
 	int					antx, anty;				// ant position
+	int					zoomx, zoomy;			// current zoom start x,y
 	char				antdir;					// ant direction
 
 	SDL_Init(SDL_INIT_EVERYTHING);
@@ -34,19 +36,22 @@ int main(int argc, char **argv) {
 
 	fieldSurface = SDL_CreateRGBSurface(SDL_HWSURFACE, FWIDTH, FHEIGHT, 32, 0, 0, 0, 0);
 	heatmapSurface = SDL_CreateRGBSurface(SDL_HWSURFACE, FWIDTH, FHEIGHT, 32, 0, 0, 0, 0);
+	zoomSurface = SDL_CreateRGBSurface(SDL_HWSURFACE, FWIDTH, FHEIGHT, 32, 0, 0, 0, 0);
 
-	// initial ant position
-	antx = FWIDTH/2;
-	anty = FHEIGHT/2;
-	antdir = 3;
-
-	// init field and heatmap
+	// init arrays
 	for (int y = 0; y < FHEIGHT; y++) {
 		for (int x = 0; x < FWIDTH; x++) {
 			field[y*FWIDTH+x] = 0;
 			heatmap[y*FWIDTH+x] = 0;
 		}
 	}
+
+	// initial ant position
+	antx = FWIDTH/2;
+	anty = FHEIGHT/2;
+	antdir = 3;
+	zoomx = 0;
+	zoomy = 0;
 
 	// draw border
 	for (int x = 0; x < WIDTH; x++) {
@@ -114,13 +119,14 @@ int main(int argc, char **argv) {
 				break;
 		}
 
-		// display field
+		// update various surfaces
 		field2surface(field, fieldSurface);
 		heatmap2surface(heatmap, heatmapSurface);
+		updateZoom(antx, anty, field, zoomSurface, zoomx, zoomy);
 
 		// display ant
 		putpixel(fieldSurface, antx, anty, 255, 0, 0);
-		updatescreen(screen, fieldSurface, heatmapSurface);
+		updatescreen(screen, fieldSurface, zoomSurface, heatmapSurface);
 
 		SDL_Delay(1);	// Don't hog the CPU too much
 	}
@@ -159,7 +165,7 @@ void	heatmap2surface(unsigned int *heatmap, SDL_Surface *surface) {
 
 	for (y = 0; y < FHEIGHT; y++) {
 		for (x = 0; x < FWIDTH; x++) {
-			r = heatmap[y*FWIDTH+x] * 10;
+			r = heatmap[y*FWIDTH+x] * 5;
 			if (r > 255) {
 				r = 255;
 			}
@@ -168,7 +174,7 @@ void	heatmap2surface(unsigned int *heatmap, SDL_Surface *surface) {
 	}
 }
 
-void	updatescreen(SDL_Surface *screen, SDL_Surface *field, SDL_Surface *heatmap) {
+void	updatescreen(SDL_Surface *screen, SDL_Surface *field, SDL_Surface *zoom, SDL_Surface *heatmap) {
 	SDL_Rect	winpos;
 
 	// field
@@ -176,10 +182,58 @@ void	updatescreen(SDL_Surface *screen, SDL_Surface *field, SDL_Surface *heatmap)
 	winpos.y = 0;
 	SDL_BlitSurface(field, NULL, screen, &winpos);
 
-	// heat map
+	// zoom
 	winpos.x = FWIDTH+2;
 	winpos.y = 0;
+	SDL_BlitSurface(zoom, NULL, screen, &winpos);
+
+	// heat map
+	winpos.x = 0;
+	winpos.y = FHEIGHT+2;
 	SDL_BlitSurface(heatmap, NULL, screen, &winpos);
 
 	SDL_Flip(screen);
+}
+
+void	updateZoom(int antx, int anty, char *field, SDL_Surface *zoomSurface, int &zoomx, int &zoomy) {
+	int		x,y;
+	int		fx, fy;
+	char	fvalue;
+	int		nx, ny;
+
+	// blank the surface
+	for (y = 0; y < FHEIGHT; y++) {
+		for (x = 0; x < FWIDTH; x++) {
+			putpixel(zoomSurface, x, y, 0, 0, 0);
+		}
+	}
+
+	// should we recalculate our zoom x,y start position?
+	if (antx < zoomx || anty < zoomy) {
+		zoomx = antx - FWIDTH/2/ZOOM;
+		zoomy = anty - FHEIGHT/2/ZOOM;
+	}
+
+	// draw zoomed in field
+	y = 0;
+	for (fy = anty - FHEIGHT/2/ZOOM; fy < anty + FHEIGHT/2/ZOOM; fy++) {
+		x = 0;
+		for (fx = antx - FWIDTH/2/ZOOM; fx < antx + FWIDTH/2/ZOOM; fx++) {
+			fvalue = field[fy*FWIDTH+fx];
+			for (ny = 0; ny < ZOOM; ny++) {
+				for (nx = 0; nx < ZOOM; nx++) {
+					putpixel(zoomSurface, x*ZOOM+nx, y*ZOOM+ny, fvalue*100, fvalue*100, fvalue*100);
+				}
+			}
+			x++;
+		}
+		y++;
+	}
+
+	// draw ant in middle of surface
+	for (y = FHEIGHT/2 - ZOOM/2; y < FHEIGHT/2 + ZOOM/2; y++) {
+		for (x = FWIDTH/2 - ZOOM/2; x < FWIDTH/2 + ZOOM/2; x++) {
+			putpixel(zoomSurface, x, y, 255, 0, 0);
+		}
+	}
 }
